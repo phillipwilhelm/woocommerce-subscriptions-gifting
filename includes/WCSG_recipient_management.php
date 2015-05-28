6bb5bf17b6ae907eb9c61efde89a9f15e0b0d3a5
James Allan <james.allan@live.com>
2015-05-28 16:15:00 +1000

    Added the function for recipients to cancel and suspend Added checks before adding cancel and suspend actions to see if the subscription can be cancelled or suspended

--- includes/WCSG_recipient_management.php
+++ includes/WCSG_recipient_management.php
@@ -8,24 +8,65 @@ class WCSG_Recipient_Management{
 
 		add_filter ( 'wcs_view_subscription_actions', __CLASS__ . '::add_recipient_actions', 1, 2 );
 
+		//we want to change the current user to the owner just before maybe_change_users_subscription is called
+		add_action( 'init', __CLASS__ . '::change_user_recipient_subscription', 99 );
+
+		add_filter( 'wcs_get_view_subscription_url', __CLASS__ . '::after_change_recipient_subscription', 1, 2 );
+
 	}
 
-	public static function add_recipient_actions( $actions, $subscription ){
+	public static function add_recipient_actions( $actions, $subscription ) {
+		//check if the current user is the recipient of the subscription
+		if( $subscription->recipient_user == wp_get_current_user()->ID ) {
 
-		if($subscription->recipient_user == wp_get_current_user()->ID){
-			error_log('this is the recipient user');
-
-			$actions['cancel'] = array(
-				'url'  => wcs_get_users_change_status_link( $subscription->id, 'cancelled', $subscription->recipient_user ),
-				'name' => __( 'Cancel', 'woocommerce-subscriptions' )
-			);
-
-			$actions['suspend'] = array(
-				'url'  => wcs_get_users_change_status_link( $subscription->id, 'on-hold', $subscription->recipient_user ),
-				'name' => __( 'Suspend', 'woocommerce-subscriptions' )
-			);
+			if ( $subscription->can_be_updated_to( 'cancelled' ) ) {
+				$actions['cancel'] = array(
+					'url'  => self::get_recipient_change_status_link( $subscription->id, 'cancelled', $subscription->recipient_user ),
+					'name' => __( 'Cancel', 'woocommerce-subscriptions' )
+				);
+			}
+			if ( $subscription->can_be_updated_to( 'on-hold' ) ) {
+				$actions['suspend'] = array(
+					'url'  => self::get_recipient_change_status_link( $subscription->id, 'on-hold', $subscription->recipient_user ),
+					'name' => __( 'Suspend', 'woocommerce-subscriptions' )
+				);
+			}
 		}
 		return $actions;
+	}
+
+	private static function get_recipient_change_status_link( $subscription_id, $status, $recipient_id ) {
+
+		$subscription = wcs_get_subscription( $subscription_id );
+		$current_user = wp_get_current_user()->ID;
+
+		//temporarily set the user to the owner of the sub to generate nonce
+		wp_set_current_user( $subscription->customer_user );
+		$action_link = add_query_arg( array( 'subscription_id' => $subscription_id, 'change_subscription_to' => $status, 'wcsg_requesting_recipient_id' => $recipient_id ) );
+		$action_link = wp_nonce_url( $action_link, $subscription_id );
+		//set the user back
+		wp_set_current_user( $current_user );
+
+		return $action_link;
+	}
+
+	public static function change_user_recipient_subscription() {
+		//check if the request is being made from the recipient (wcsg_requesting_recipient_id is set)
+		if ( isset( $_GET['wcsg_requesting_recipient_id'] ) && isset( $_GET['change_subscription_to'] ) && isset( $_GET['subscription_id'] ) && isset( $_GET['_wpnonce'] ) ) {
+			$subscription = wcs_get_subscription( $_GET['subscription_id'] );
+			//temporarily assign the user to the subscription owner
+			wp_set_current_user( $subscription->customer_user );
+		}
+
+	}
+
+	public static function after_change_recipient_subscription( $url, $subscription_id ) {
+		if ( isset( $_GET['wcsg_requesting_recipient_id'] ) ) {
+			$subscription = wcs_get_subscription( $subscription_id );
+			wp_set_current_user( $subscription->recipient_user );
+		}
+
+		return $url;
 	}
 
 	public static function add_recipient_subscriptions( $subscriptions, $user_id ) {
@@ -53,16 +94,14 @@ class WCSG_Recipient_Management{
 
 	public static function gifting_information_after_customer_details( $subscription ){
 		//check if the subscription is gifted
-		if( !empty($subscription->recipient_user) ) {
+		if ( ! empty( $subscription->recipient_user ) ) {
 			$customer_user = new WP_User( $subscription->customer_user );
 			$recipient_user = new WP_User( $subscription->recipient_user );
 			$current_user = wp_get_current_user();
 
 			if( $current_user->ID == $customer_user->ID ){
-				//display the recipient information
 				echo self::add_gifting_information_html( $recipient_user->first_name . ' ' . $recipient_user->last_name, 'Recipient' );
 			}else{
-				//display the purchaser information
 				echo self::add_gifting_information_html( $customer_user->first_name . ' ' . $customer_user->last_name, 'Purchaser' );
 			}
 		}
