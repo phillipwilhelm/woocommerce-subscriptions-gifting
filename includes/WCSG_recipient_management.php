d649231d5c62741f04dd02fb51761284c52142b5
James Allan <james.allan@live.com>
2015-06-09 13:28:54 +1000

    Changed the way recipients cancel and suspend gifted subscriptions to reflect the changes made to subscriptions core.

--- includes/WCSG_recipient_management.php
+++ includes/WCSG_recipient_management.php
@@ -8,27 +8,25 @@ class WCSG_Recipient_Management{
 
 		add_filter ( 'wcs_view_subscription_actions', __CLASS__ . '::add_recipient_actions', 1, 2 );
 
-		//we want to change the current user to the owner just before maybe_change_users_subscription is called
+		//we want to handle changing subs status before subscriptions core
 		add_action( 'init', __CLASS__ . '::change_user_recipient_subscription', 99 );
 
-		add_filter( 'wcs_get_view_subscription_url', __CLASS__ . '::after_change_recipient_subscription', 1, 2 );
-
+		add_filter( 'wcs_can_user_put_subscription_on_hold' , __CLASS__ . '::recipient_can_suspend', 1, 2 );
 	}
 
 	public static function add_recipient_actions( $actions, $subscription ) {
-		//check if the current user is the recipient of the subscription
-		if( $subscription->recipient_user == wp_get_current_user()->ID ) {
 
-			if ( $subscription->can_be_updated_to( 'cancelled' ) ) {
-				$actions['cancel'] = array(
-					'url'  => self::get_recipient_change_status_link( $subscription->id, 'cancelled', $subscription->recipient_user ),
-					'name' => __( 'Cancel', 'woocommerce-subscriptions' )
-				);
-			}
+		if ( $subscription->recipient_user == wp_get_current_user()->ID ) {
 			if ( $subscription->can_be_updated_to( 'on-hold' ) ) {
 				$actions['suspend'] = array(
 					'url'  => self::get_recipient_change_status_link( $subscription->id, 'on-hold', $subscription->recipient_user ),
 					'name' => __( 'Suspend', 'woocommerce-subscriptions' )
+				);
+			}
+			if ( $subscription->can_be_updated_to( 'cancelled' ) ) {
+				$actions['cancel'] = array(
+					'url'  => self::get_recipient_change_status_link( $subscription->id, 'cancelled', $subscription->recipient_user ),
+					'name' => __( 'Cancel', 'woocommerce-subscriptions' )
 				);
 			}
 		}
@@ -37,15 +35,8 @@ class WCSG_Recipient_Management{
 
 	private static function get_recipient_change_status_link( $subscription_id, $status, $recipient_id ) {
 
-		$subscription = wcs_get_subscription( $subscription_id );
-		$current_user = wp_get_current_user()->ID;
-
-		//temporarily set the user to the owner of the sub to generate nonce
-		wp_set_current_user( $subscription->customer_user );
 		$action_link = add_query_arg( array( 'subscription_id' => $subscription_id, 'change_subscription_to' => $status, 'wcsg_requesting_recipient_id' => $recipient_id ) );
 		$action_link = wp_nonce_url( $action_link, $subscription_id );
-		//set the user back
-		wp_set_current_user( $current_user );
 
 		return $action_link;
 	}
@@ -53,24 +44,41 @@ class WCSG_Recipient_Management{
 	public static function change_user_recipient_subscription() {
 		//check if the request is being made from the recipient (wcsg_requesting_recipient_id is set)
 		if ( isset( $_GET['wcsg_requesting_recipient_id'] ) && isset( $_GET['change_subscription_to'] ) && isset( $_GET['subscription_id'] ) && isset( $_GET['_wpnonce'] ) ) {
-			$subscription = wcs_get_subscription( $_GET['subscription_id'] );
-			//temporarily assign the user to the subscription owner
-			wp_set_current_user( $subscription->customer_user );
+
+			remove_action( 'init', 'WCS_User_Change_Status_Handler::maybe_change_users_subscription', 100 );
+
+			$subscription 	= wcs_get_subscription( $_GET['subscription_id'] );
+			$user_id 		= $subscription->get_user_id();
+			$new_status 	= $_GET['change_subscription_to'];
+
+			if ( WCS_User_Change_Status_Handler::validate_request( $user_id, $subscription, $new_status, $_GET['_wpnonce'] ) ) {
+				WCS_User_Change_Status_Handler::change_users_subscription( $subscription, $new_status );
+				wp_safe_redirect( $subscription->get_view_order_url() );
+				exit;
+			}
 		}
 
 	}
 
-	public static function after_change_recipient_subscription( $url, $subscription_id ) {
-		if ( isset( $_GET['wcsg_requesting_recipient_id'] ) ) {
-			$subscription = wcs_get_subscription( $subscription_id );
-			wp_set_current_user( $subscription->recipient_user );
+	public static function recipient_can_suspend(  $user_can_suspend, $subscription ){
+
+		if ( $subscription->recipient_user == wp_get_current_user()->ID ){
+
+			// Make sure subscription suspension count hasn't been reached
+			$suspension_count    = $subscription->suspension_count;
+			$allowed_suspensions = get_option( WC_Subscriptions_Admin::$option_prefix . '_max_customer_suspensions', 0 );
+
+			if ( 'unlimited' === $allowed_suspensions || $allowed_suspensions > $suspension_count ) { // 0 not > anything so prevents a customer ever being able to suspend
+				$user_can_suspend = true;
+			}
 		}
 
-		return $url;
+		return $user_can_suspend;
+
 	}
 
 	public static function add_recipient_subscriptions( $subscriptions, $user_id ) {
-		//get the posts that have been gifted to this user
+		//get the subscription posts that have been gifted to this user
 		$post_ids = get_posts( array(
 			'posts_per_page' => -1,
 			'post_status'    => 'any',
