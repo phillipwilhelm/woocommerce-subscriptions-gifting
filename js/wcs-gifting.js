dd83501855825fca7e798d8b2d023fd4dc426025
James Allan <james.allan@live.com>
2015-06-19 11:26:53 +1000

    Changed the trigger event of the checkbox to change instead if click

--- js/wcs-gifting.js
+++ js/wcs-gifting.js
@@ -1,9 +1,11 @@
 jQuery(document).ready(function($){
-	$(document).on('click', '.woocommerce_subscription_gifting_checkbox',function() {
+	$(document).on('change', '.woocommerce_subscription_gifting_checkbox',function(e) {
 		if ($(this).is(':checked')) {
 			$(this).parents().children('.woocommerce_subscriptions_gifting_recipient_email').slideDown( 250 );
 		} else {
 			$(this).parents().children('.woocommerce_subscriptions_gifting_recipient_email').slideUp( 250 );
+			$(this).parents().children('.woocommerce_subscriptions_gifting_recipient_email').val('');
 		}
+		e.preventDefault();
 	});
 });
