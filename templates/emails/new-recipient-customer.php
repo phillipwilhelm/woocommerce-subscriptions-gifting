e0c1074fcc22368c9de7a0238c714a2920cdddf6
James Allan <james.allan@live.com>
2015-07-07 15:22:55 +1000

    Add escaping to html email templates

--- templates/emails/new-recipient-customer.php
+++ templates/emails/new-recipient-customer.php
@@ -13,13 +13,13 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 <?php do_action( 'woocommerce_email_header', $email_heading ); ?>
 
-<p><?php printf( __( "Hi there,", 'woocommerce-subscriptions-gifting' ) ); ?></p>
-<p><?php printf( __( "%s just purchased a subscription for you at %s so we've created an account for you to manage the subscription.", 'woocommerce-subscriptions-gifting' ), esc_html( $subscription_purchaser ), esc_html( $blogname ) ); ?></p>
+<p><?php printf( esc_html__( "Hi there,", 'woocommerce-subscriptions-gifting' ) ); ?></p>
+<p><?php printf( esc_html__( "%s just purchased a subscription for you at %s so we've created an account for you to manage the subscription.", 'woocommerce-subscriptions-gifting' ), esc_html( $subscription_purchaser ), esc_html( $blogname ) ); ?></p>
 
-<p><?php printf( __( "Your username is: <strong>%s</strong>", 'woocommerce-subscriptions-gifting' ), esc_html( $user_login ) ); ?></p>
-<p><?php printf( __( "Your password has been automatically generated: <strong>%s</strong>", 'woocommerce-subscriptions-gifting' ), esc_html( $user_password ) ); ?></p>
+<p><?php printf( esc_html__( "Your username is: <strong>%s</strong>", 'woocommerce-subscriptions-gifting' ), esc_html( $user_login ) ); ?></p>
+<p><?php printf( esc_html__( "Your password has been automatically generated: <strong>%s</strong>", 'woocommerce-subscriptions-gifting' ), esc_html( $user_password ) ); ?></p>
 
-<p><?php printf( __( 'To complete your account we just need your shipping address and you to change your password here: %s.', 'woocommerce-subscriptions-gifting' ), wc_get_page_permalink( 'myaccount' ) . 'new-recipient-account/' ); ?></p>
-<p><?php printf( __( 'Once completed you may access your account area to view your subscription here: %s.', 'woocommerce-subscriptions-gifting' ), wc_get_page_permalink( 'myaccount' ) ); ?></p>
+<p><?php printf( esc_html__( 'To complete your account we just need your shipping address and you to change your password here: %s.', 'woocommerce-subscriptions-gifting' ), wc_get_page_permalink( 'myaccount' ) . 'new-recipient-account/' ); ?></p>
+<p><?php printf( esc_html__( 'Once completed you may access your account area to view your subscription here: %s.', 'woocommerce-subscriptions-gifting' ), wc_get_page_permalink( 'myaccount' ) ); ?></p>
 
 <?php do_action( 'woocommerce_email_footer' ); ?>
