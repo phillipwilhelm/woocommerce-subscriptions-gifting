2a136de94287b9f9caefa589c9e563a7c42bc4c6
James Allan <james.allan@live.com>
2015-07-17 13:16:09 +1000

    Changes to the checkout recipient email validation

--- includes/class-wcsg-checkout.php
+++ includes/class-wcsg-checkout.php
@@ -26,7 +26,14 @@ class WCSG_Checkout {
 	 */
 	public static function add_gifting_option_checkout( $quantity, $cart_item, $cart_item_key ) {
 		if ( WC_Subscriptions_Product::is_subscription( $cart_item['data'] ) ) {
-			$email = ( ! isset( $cart_item['wcsg_gift_recipients_email'] ) ) ? '' : $cart_item['wcsg_gift_recipients_email'];
+
+			$email = '';
+			if ( ! empty( $_POST['recipient_email'][ $cart_item_key ] ) ) {
+				$email = $_POST['recipient_email'][ $cart_item_key ];
+			} else if ( ! empty( $cart_item['wcsg_gift_recipients_email'] ) ) {
+				$email = $cart_item['wcsg_gift_recipients_email'];
+			}
+
 			$quantity .= WCS_Gifting::generate_gifting_html( $cart_item_key, $email );
 		}
 		return $quantity;
@@ -90,19 +97,25 @@ class WCSG_Checkout {
 
 	public static function check_recipient_email() {
 		if ( ! empty( $_POST['recipient_email'] ) ) {
-			$recipients = $_POST['recipient_email'];
-
-			foreach ( $recipients as $recipient ) {
+			$recipients          = $_POST['recipient_email'];
+			$invalid_email_found = false;
+			$self_gifting_found  = false;
+			foreach ( $recipients as $key => $recipient ) {
+				//change to the update cart function once it is merged with master.
+				WC()->cart->cart_contents[ $key ]['wcsg_gift_recipients_email'] = $recipient;
 				$recipient = sanitize_email( $recipient );
 				if ( is_email( $recipient ) ) {
-					if ( WCS_Gifting::recipient_email_is_current_user( $recipient ) ) {
+					if ( ! $self_gifting_found && WCS_Gifting::recipient_email_is_current_user( $recipient ) ) {
 						wc_add_notice( __( 'You cannot gift a product to yourself.', 'woocommerce-subscriptions-gifting' ), 'error' );
-						break;
+						$self_gifting_found = true;
 					}
-				} else {
-					wc_add_notice( __( 'Invalid email address.', 'woocommerce-subscriptions-gifting' ), 'error' );
-					break;
+				} else if ( ! $invalid_email_found ) {
+					wc_add_notice( __( ' Invalid email address.', 'woocommerce-subscriptions-gifting' ), 'error' );
+					$invalid_email_found = true;
 				}
+			}
+			if ( $invalid_email_found || $self_gifting_found ) {
+				WC()->session->set( 'reload_checkout', true );
 			}
 		}
 	}
