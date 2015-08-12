515bfa8a15581aec856d1021818126d80547d071
Jason Conroy <jason@findingsimple.com>
2015-08-12 14:35:01 +1000

    Use a single command for clone and checkout

--- tests/bin/travis.sh
+++ tests/bin/travis.sh
@@ -8,9 +8,6 @@ if [ $1 == 'before' ]; then
 	curl -L https://api.github.com/repos/woothemes/woocommerce/tarball/$WC_VERSION?access_token=$GITHUB_TOKEN | tar --strip-components=1 -zx -C "../woocommerce"
 
 	# place a copy of woocommerce subscriptions where the unit tests etc. expect it to be - needs to be a repo with the test dir
-	git clone https://$GITHUB_TOKEN@github.com/Prospress/woocommerce-subscriptions.git "../woocommerce-subscriptions"
-	cd "../woocommerce-subscriptions"
-	git checkout $WCS_VERSION
-	cd "../woocommerce-subscriptions-gifting"
+	git clone https://$GITHUB_TOKEN@github.com/Prospress/woocommerce-subscriptions.git "../woocommerce-subscriptions" -b $WCS_VERSION
 
 fi
