3011e03381638bee8cfdbec3f33a4924c060772a
James Allan <james.allan@live.com>
2015-09-01 14:55:24 +1000

    Clears the text field when unchecking the checkbox

--- js/wcs-gifting.js
+++ js/wcs-gifting.js
@@ -1,9 +1,10 @@
 jQuery(document).ready(function($){
 	$(document).on('click', '.woocommerce_subscription_gifting_checkbox',function() {
 		if ($(this).is(':checked')) {
-			$(this).parents().children('.woocommerce_subscriptions_gifting_recipient_email').slideDown( 250 );
+			$(this).siblings('.woocommerce_subscriptions_gifting_recipient_email').slideDown( 250 );
 		} else {
-			$(this).parents().children('.woocommerce_subscriptions_gifting_recipient_email').slideUp( 250 );
+			$(this).siblings('.woocommerce_subscriptions_gifting_recipient_email').slideUp( 250 );
+			$(this).parents().children().children('.recipient_email').val('');
 		}
 	});
 });
