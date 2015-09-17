5ba3c91d1d168226140e433d034ccddfa03bf52a
James Allan <james.allan@live.com>
2015-09-17 18:01:21 +1000

    Adds various unit tests

--- tests/unit/wcsg-unit-test-case.php
+++ tests/unit/wcsg-unit-test-case.php
@@ -6,16 +6,21 @@
  */
 class WCSG_Unit_Test_Case extends WC_Unit_Test_Case {
 
+	public $wcsg_test_product_one;
+	public $wcsg_test_product_two;
+
+	public function setUp() {
+
+		$this->wcsg_test_product_one = WCS_Helper_Product::create_simple_subscription_product();
+		$this->wcsg_test_product_two = WCS_Helper_Product::create_simple_subscription_product();
+	}
+
+
 	public function test_update_cart_item_key() {
 
 		/*********************************SETUP CART*********************************/
 
-		add_filter( 'woocommerce_subscription_is_purchasable', '__return_true' );
-
-		$product = WCS_Helper_Product::create_simple_subscription_product();
-		WC()->cart->add_to_cart( $product->id, 1 );
-
-		remove_filter( 'woocommerce_subscription_is_purchasable', '__return_true' );
+		self::add_to_test_cart( $this->wcsg_test_product_one, 1 );
 
 		/****************************************************************************/
 
@@ -51,14 +56,14 @@ class WCSG_Unit_Test_Case extends WC_Unit_Test_Case {
 
 		/****************************************************************************/
 
-		WC()->cart->add_to_cart( $product->id, 1 );
+		self::add_to_test_cart( $this->wcsg_test_product_one, 1 );
 
 		$cart_item = reset( WC()->cart->cart_contents );
 
 		$this->assertTrue( 1 == count( WC()->cart->cart_contents ) );
 		$this->assertTrue( 2 == $cart_item['quantity'] );
 
-		WC()->cart->add_to_cart( $product->id, 3 );
+		self::add_to_test_cart( $this->wcsg_test_product_one, 3 );
 
 		$cart_item = reset( WC()->cart->cart_contents );
 
@@ -73,7 +78,7 @@ class WCSG_Unit_Test_Case extends WC_Unit_Test_Case {
 
 		WCS_Gifting::update_cart_item_key( $cart_item, $cart_item_key, 'email@example.com' );
 
-		WC()->cart->add_to_cart( $product->id, 1 );
+		self::add_to_test_cart( $this->wcsg_test_product_one, 1 );
 
 		$this->assertTrue( 2 == count( WC()->cart->cart_contents ) );
 
@@ -82,10 +87,8 @@ class WCSG_Unit_Test_Case extends WC_Unit_Test_Case {
 		//clear the cart
 		WC()->cart->empty_cart();
 
-		$product_two = WCS_Helper_Product::create_simple_subscription_product();
-
-		WC()->cart->add_to_cart( $product->id, 1 );
-		WC()->cart->add_to_cart( $product_two->id, 1 );
+		self::add_to_test_cart( $this->wcsg_test_product_one, 1 );
+		self::add_to_test_cart( $this->wcsg_test_product_two, 1 );
 
 		$this->assertTrue( 2 == count( WC()->cart->cart_contents ) );
 
@@ -101,9 +104,145 @@ class WCSG_Unit_Test_Case extends WC_Unit_Test_Case {
 		WCS_Gifting::update_cart_item_key( $cart_item_two, $cart_item_two_key, 'email@example.com' );
 
 		$this->assertTrue( 2 == count( WC()->cart->cart_contents ) );
+
+		/****************************************************************************/
+
+		//clean up
+		WC()->cart->empty_cart();
 	}
 
-	public function test() {
+	public function test_email_belongs_to_current_user() {
+
+		$user_id = wp_create_user( 'user', 'password', 'email@example.com' );
+
+		wp_set_current_user( $user_id );
+
+		$this->assertTrue( WCS_Gifting::email_belongs_to_current_user( 'email@example.com' ) );
+		$this->assertTrue( false == WCS_Gifting::email_belongs_to_current_user( '' ) );
+		$this->assertTrue( false == WCS_Gifting::email_belongs_to_current_user( 'email1@example.com' ) );
+	}
+
+	/**
+	 * Basic tests for WCSG_Checkout::add_recipient_email_recurring_cart_key
+	 *
+	 * @dataProvider recipient_recurring_cart_keys_setup
+	 */
+	public function test_recipient_recurring_cart_keys( $products, $expected ) {
+
+		foreach ( $products as $product ) {
+			self::add_to_test_cart( $product['product'] , 1 );
+			$cart_item_keys = array_keys( WC()->cart->cart_contents );
+			WC()->cart->cart_contents[ end( $cart_item_keys ) ]['wcsg_gift_recipients_email'] = $product['recipient_email'];
+		}
+
+		//setup recurring carts
+		WC_Subscriptions_Cart::calculate_subscription_totals( 0 , WC()->cart );
+
+		$this->assertEquals( $expected, count( WC()->cart->recurring_carts ) );
+
+		//clean up
+		WC()->cart->empty_cart();
+	}
+
+	public static function recipient_recurring_cart_keys_setup() {
+
+		$daily_subscription_product = WCS_Helper_Product::create_simple_subscription_product( array( 'subscription_period' => 'day' ) );
+
+		$monthly_subscription_product = WCS_Helper_Product::create_simple_subscription_product( array( 'subscription_period' => 'month' ) );
+		$monthly_subscription_product_two = WCS_Helper_Product::create_simple_subscription_product( array( 'subscription_period' => 'month' ) );
+
+		return array(
+			//1 Monthly Product, 1 recipient. Expects 1 recurring cart.
+			array(
+				'products' => array(
+					array( 'product' => $monthly_subscription_product, 'recipient_email' => 'email@example.com' ),
+					)
+			, 1 ),
+			//2 Monthly Products, 1 recipient. Expects 1 recurring cart.
+			array(
+				'products' => array(
+					array( 'product' => $monthly_subscription_product, 'recipient_email' => 'email@example.com' ),
+					array( 'product' => $monthly_subscription_product_two, 'recipient_email' => 'email@example.com' ),
+					)
+			, 1 ),
+			//2 Monthly Products, 2 recipients. Expects 2 recurring carts.
+			array(
+				'products' => array(
+					array( 'product' => $monthly_subscription_product, 'recipient_email' => 'email_two@example.com' ),
+					array( 'product' => $monthly_subscription_product_two, 'recipient_email' => 'email@example.com' ),
+					)
+			, 2 ),
+			//1 Monthly and 1 daily, 1 recipient. Expects 2 recurring carts.
+			array(
+				'products' => array(
+					array( 'product' => $daily_subscription_product, 'recipient_email' => 'email@example.com' ),
+					array( 'product' => $monthly_subscription_product_two, 'recipient_email' => 'email@example.com' ),
+					)
+			, 2 ),
+			//1 Monthly and 1 daily, 2 recipient. Expects 2 recurring carts.
+			array(
+				'products' => array(
+					array( 'product' => $daily_subscription_product, 'recipient_email' => 'email_two@example.com' ),
+					array( 'product' => $monthly_subscription_product_two, 'recipient_email' => 'email@example.com' ),
+					)
+			, 2 ),
+		);
+	}
+
+	public function test_user_display_name() {
+
+		$user_id = wp_create_user( 'user_one', 'testuser', 'user_one@example.com' );
+
+		//Just email
+		$this->assertEquals( 'user_one@example.com', WCS_Gifting::get_user_display_name( $user_id ) );
+
+		//First name and Email
+		update_user_meta( $user_id, 'first_name', 'first_name');
+		$this->assertEquals( 'first_name (user_one@example.com)', WCS_Gifting::get_user_display_name( $user_id ) );
+
+		//First name, last name and Email
+		update_user_meta( $user_id, 'last_name', 'last_name');
+		$this->assertEquals( 'first_name last_name (user_one@example.com)', WCS_Gifting::get_user_display_name( $user_id ) );
+
+		//cleanup
+		wp_delete_user( $user_id );
+	}
+
+	public function test_add_recipient_subscriptions() {
+
+		$recipient_user     = wp_create_user( 'recipient_user', 'password', 'recipient@example.com' );
+
+		//Gifted Subscription
+		$subscription       = WCS_Helper_Subscription::create_subscription( array(), array( 'recipient_user' => $recipient_user ) );
+		$user_subscriptions = wcs_get_users_subscriptions( $recipient_user );
+
+		$this->assertTrue( array_key_exists( $subscription->id, $user_subscriptions ) );
+		$this->assertEquals( 1 , count( $user_subscriptions ) );
+
+		//Purchased Subscription
+		$subscription_two   = WCS_Helper_Subscription::create_subscription( array( 'customer_id' => $recipient_user ) );
+		$user_subscriptions = wcs_get_users_subscriptions( $recipient_user );
+
+		$this->assertTrue( array_key_exists( $subscription->id, $user_subscriptions ) );
+		$this->assertTrue( array_key_exists( $subscription_two->id, $user_subscriptions ) );
+
+		$this->assertEquals( 2 , count( $user_subscriptions ) );
+
+		//clean up
+		wp_delete_post( $subscription->id, true );
+		wp_delete_post( $subscription_two->id, true );
+
+		wp_delete_user( $recipient_user );
+	}
+
+
+	public static function add_to_test_cart( $product, $quantity ) {
+
+		add_filter( 'woocommerce_subscription_is_purchasable', '__return_true' );
+
+		WC()->cart->add_to_cart( $product->id, $quantity );
+
+		remove_filter( 'woocommerce_subscription_is_purchasable', '__return_true' );
 
 	}
 }
