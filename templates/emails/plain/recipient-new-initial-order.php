36f975a8f5e1a3f62b47da56f7a26ab912fcc708
Jason Conroy <jason@findingsimple.com>
2016-09-04 20:27:19 +1000

    The $text arg must be a single string literal

--- templates/emails/plain/recipient-new-initial-order.php
+++ templates/emails/plain/recipient-new-initial-order.php
@@ -11,15 +11,15 @@ if ( ! defined( 'ABSPATH' ) ) {
 echo '= ' . $email_heading . " =\n\n";
 echo sprintf( __( 'Hi there,', 'woocommerce-subscriptions-gifting' ) ) . "\n";
 // translators: 1$: Purchaser's name and email, 2$ The name of the site.
-echo sprintf( __( '%1$s just purchased ' . _n( 'a subscription', 'subscriptions', count( $subscriptions ), 'woocommerce-subscriptions-gifting' ) . ' for you at %2$s.', 'woocommerce-subscriptions-gifting' ), wp_kses( $subscription_purchaser, wp_kses_allowed_html( 'user_description' ) ), esc_html( $blogname ) );
-echo sprintf( __( ' Details of the ' . _n( 'subscription', 'subscriptions', count( $subscriptions ), 'woocommerce-subscriptions-gifting' ) . ' are shown below.', 'woocommerce-subscriptions-gifting' ) ) . "\n\n";
+echo sprintf( __( '%1$s just purchased %2$s for you at %3$s.', 'woocommerce-subscriptions-gifting' ), wp_kses( $subscription_purchaser, wp_kses_allowed_html( 'user_description' ) ), _n( 'a subscription', 'subscriptions', count( $subscriptions ), 'woocommerce-subscriptions-gifting' ), esc_html( $blogname ) );
+echo sprintf( __( ' Details of the %s are shown below.', 'woocommerce-subscriptions-gifting' ), _n( 'subscription', 'subscriptions', count( $subscriptions ), 'woocommerce-subscriptions-gifting' ) ) . "\n\n";
 
 $new_recipient = get_user_meta( $recipient_user->ID, 'wcsg_update_account', true );
 
 if ( 'true' == $new_recipient ) {
 	echo esc_html__( 'We noticed you didn\'t have an account so we created one for you. Your account login details will have been sent to you in a separate email.' ) . "\n\n";
 } else {
-	echo sprintf( __( 'You may access your account area to view your new ' . _n( 'subscription', 'subscriptions', count( $subscriptions ), 'woocommerce-subscriptions-gifting' ) . ' here: %s.', 'woocommerce-subscriptions-gifting' ), esc_url( wc_get_page_permalink( 'myaccount' ) ) ) . "\n\n";
+	echo sprintf( __( 'You may access your account area to view your new %1$s here: %2$s.', 'woocommerce-subscriptions-gifting' ), _n( 'subscription', 'subscriptions', count( $subscriptions ), 'woocommerce-subscriptions-gifting' ), esc_url( wc_get_page_permalink( 'myaccount' ) ) ) . "\n\n";
 }
 
 foreach ( $subscriptions as $subscription_id ) {
