b0f36e1409762ef59cb361bd3bcb661e9655b822
Jason Conroy <jason@findingsimple.com>
2016-09-04 20:46:09 +1000

    The $text arg must be a single string literal

--- templates/html-add-recipient.php
+++ templates/html-add-recipient.php
@@ -1,11 +1,11 @@
 <fieldset>
-	<input type="checkbox" id="gifting_<?php esc_attr_e( $id, 'woocommerce_subscriptions_gifting' ) ?>_option" class="woocommerce_subscription_gifting_checkbox" value="gift" <?php esc_attr_e( ( empty( $email ) ) ? '' : 'checked', 'woocommerce_subscriptions_gifting' ) ?> />
+	<input type="checkbox" id="gifting_<?php echo esc_attr( $id ); ?>_option" class="woocommerce_subscription_gifting_checkbox" value="gift" <?php echo esc_attr( ( empty( $email ) ) ? '' : 'checked' ); ?> />
 	<?php echo esc_html( apply_filters( 'wcsg_enable_gifting_checkbox_label', get_option( WCSG_Admin::$option_prefix . '_gifting_checkbox_text', __( 'This is a gift', 'woocommerce_subscriptions_gifting' ) ) ) ); ?> <br />
-	<p class="form-row form-row <?php esc_attr_e( implode( ' ', $email_field_args['class'] ) ); ?>" style="<?php esc_attr_e( implode( '; ', $email_field_args['style_attributes'] ) );?>">
-		<label for="recipient_email[<?php esc_attr_e( $id );?>]">
+	<p class="form-row form-row <?php echo esc_attr( implode( ' ', $email_field_args['class'] ) ); ?>" style="<?php echo esc_attr( implode( '; ', $email_field_args['style_attributes'] ) ); ?>">
+		<label for="recipient_email[<?php echo esc_attr( $id ); ?>]">
 			<?php esc_html_e( "Recipient's Email Address:", 'woocommerce-subscriptions-gifting' ); ?>
 		</label>
-		<input type="email" class="input-text recipient_email" name="recipient_email[<?php esc_attr_e( $id );?>]" id="recipient_email[<?php esc_attr_e( $id );?>]" placeholder="<?php esc_attr_e( $email_field_args['placeholder'] );?>" value="<?php esc_attr_e( $email )?>"/>
+		<input type="email" class="input-text recipient_email" name="recipient_email[<?php echo esc_attr( $id ); ?>]" id="recipient_email[<?php echo esc_attr( $id ); ?>]" placeholder="<?php echo esc_attr( $email_field_args['placeholder'] ); ?>" value="<?php echo esc_attr( $email ); ?>"/>
 		<?php wp_nonce_field( 'wcsg_add_recipient', '_wcsgnonce' ); ?>
 	</p>
 </fieldset>
